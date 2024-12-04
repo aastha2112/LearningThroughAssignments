52bbd36e750c6c552bd72310983522f4d335b8be
Aastha Lakhanpal <aasthalakhanpal11@gmail.com>
2024-12-04 22:07:08 +0530

    Added: Arrow function question

--- index.js
+++ index.js
@@ -6,8 +6,16 @@ const multiply = (a = 1, b = 1) => {
   return a * b;
 };
 
-// Example Input (with defaults):
 console.log(multiply()); //1
-
-// Example Output (with defaults):
 console.log(multiply(3, 4)); //12
+
+// -------------------------------------
+
+// Question 12:
+// L0 - Square and Cube of a Number with Implicit Return
+// Write an arrow function that takes a number as input and returns both its square and cube in an object, using implicit return.
+
+const squareAndCube = (num) =>
+  console.log({ square: num ** 2, cube: num ** 3 });
+
+squareAndCube(5); //{ square: 25, cube: 125 }
