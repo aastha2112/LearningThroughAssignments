f4b16f986d790fc08e2a5e55d5f1dcdc164872e3
Aastha Lakhanpal <aasthalakhanpal11@gmail.com>
2024-12-06 22:53:54 +0530

    Employee Performance Evaluation System

--- HigherOderFunc/index.js
+++ HigherOderFunc/index.js
@@ -81,3 +81,52 @@ function arrayOfSortedcategories(Obj) {
   return sortedArray;
 }
 console.log(arrayOfSortedcategories(resultObj)); //[ 'toys', 'electronics', 'clothing' ]
+
+//-------------------------
+
+// Question 14:
+// L1 - Employee Performance Evaluation System
+
+let employeeData = [
+  { name: "Alice", tasksCompleted: 8, rating: 4.7 },
+
+  { name: "Bob", tasksCompleted: 4, rating: 4.0 },
+
+  { name: "Charlie", tasksCompleted: 6, rating: 3.5 },
+
+  { name: "David", tasksCompleted: 10, rating: 4.9 },
+
+  { name: "Eve", tasksCompleted: 7, rating: 2.8 },
+];
+function evaluateEmployee(employeeData) {
+  let filteredEmployees = employeeData.filter(
+    (employee) => employee.tasksCompleted > 5
+  );
+
+  let empAndPerformance = filteredEmployees.map((emp) => {
+    let PerformanceLvl;
+
+    if (emp.rating > 4.5) {
+      PerformanceLvl = "Excellent";
+    } else if (emp.rating >= 3 && emp.rating <= 4.5) {
+      PerformanceLvl = "Good";
+    } else {
+      PerformanceLvl = "Needs Improvement";
+    }
+
+    return { name: emp.name, performance: PerformanceLvl };
+  });
+
+  let sortedEmployees = empAndPerformance.sort((a, b) => {
+    const performanceOrder = { Excellent: 3, Good: 2, "Needs Improvement": 1 };
+    return performanceOrder[b.performance] - performanceOrder[a.performance];
+  });
+  return sortedEmployees;
+}
+
+console.log(evaluateEmployee(employeeData)); //[
+//     { name: 'Alice', performance: 'Excellent' },
+//     { name: 'David', performance: 'Excellent' },
+//     { name: 'Charlie', performance: 'Good' },
+//     { name: 'Eve', performance: 'Needs Improvement' }
+//   ]
