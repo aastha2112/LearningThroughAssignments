2ee1199a9696291d7e8c392f51628888b0f8e36d
Aastha Lakhanpal <aasthalakhanpal11@gmail.com>
2024-12-06 20:38:01 +0530

    Filter and Sort Data

--- HigherOderFunc/index.js
+++ HigherOderFunc/index.js
@@ -18,3 +18,31 @@ function processProducts(arr) {
 }
 
 processProducts(arr);
+
+//----------------------
+
+// Question 12:
+// L0 - Filter and Sort Data with Higher-Order Functions
+
+function processStudents(array) {
+  let scoreAbove60 = array.filter((obj) => {
+    if (obj.marks > 60) {
+      return obj;
+    }
+  });
+  scoreAbove60.sort((stu1, stu2) => stu2.marks - stu1.marks);
+  let sortedNames = scoreAbove60.map((student) => student.name);
+  return sortedNames;
+}
+
+let arrOfStudents = [
+  { name: "Alice", marks: 58 },
+
+  { name: "Bob", marks: 85 },
+
+  { name: "Charlie", marks: 92 },
+
+  { name: "David", marks: 45 },
+];
+
+console.log(processStudents(arrOfStudents));
